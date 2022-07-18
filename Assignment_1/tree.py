9ef13678c4aa5c90b978083f4d0033058de1453c
ahmeddaker <ahmeddakerwork@gmail.com>
2022-07-18 23:00:59 +0200

    adding a new node to the tree

--- Assignment_1/tree.py
+++ Assignment_1/tree.py
@@ -1,15 +1,16 @@
 class Node:
-	def __init__(self, key):
-		self.left = None
-		self.right = None
-		self.val = key
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
 
 if __name__ == '__main__':
-	root = Node(1)
-	root.left = Node(2)
-	root.right = Node(3)
-	root.left.left = Node(4)
-	root.left.right = Node(5)
-	root.right.left = Node(6)
-	root.right.right = Node(7)
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.left.left.left = Node(8)
